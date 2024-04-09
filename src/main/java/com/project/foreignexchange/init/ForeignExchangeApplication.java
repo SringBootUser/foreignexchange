9bb365ea40df2c73be21c18307829f3a3be8c170
SpringBootUser <k.drenski91@aol.com>
2024-04-09 23:41:11 +0300

    adding app class and global exception handler

---
+++ src/main/java/com/project/foreignexchange/init/ForeignExchangeApplication.java
@@ -0,0 +1,24 @@
+package com.project.foreignexchange.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan("com.project.foreignexchange.*")
+@EntityScan("com.project.foreignexchange.*")
+@EnableJpaRepositories("com.project.foreignexchange.*")
+@EnableFeignClients("com.project.foreignexchange.*")
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
+public class ForeignExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeignExchangeApplication.class, args);
+	}
+
+}
